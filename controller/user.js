4a65cd3851b5cd333ec7d2cfacd4640e528b4508
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-01-09 22:35:38 +0100

    update repository to typescript

--- controller/user.js
+++
@@ -1,8 +0,0 @@
-// your controller do the logic and return the result
-
-// accept two parameters: req and res
-const user = async (req, res) => {
-    res.status(200).send("Biruk Endris");
-};
-
-export default user;
--- index.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'; // allow to use .env file
-import app from './app.js' // import app.js 
-
-// choose any port you want
-const PORT = 5000;
-
-// start server on port 5000 or any port you specified in .env file
-app.listen( process.env.PORT || PORT, () => { console.log(`Server started on port ${PORT}`)})
